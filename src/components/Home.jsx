196ec4ee626be56b8a1a823737fbfe818b5d327a
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-22 17:47:22 +0530

    improved

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,34 +6,28 @@ import { GoogleUserDataContext } from "../context/context";
 // react-router-dom
 import { useNavigate } from "react-router-dom";
 
-
-
 export const Home = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const { data, logout } = useContext(GoogleUserDataContext);
 
-    const {data, logout} = useContext(GoogleUserDataContext)
-    
-    const handleUserLogout = () => {
-        logout()
-        navigate("/login")
-    }
-    
-    
-    return (
+  const handleUserLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <>
+      {data && (
         <>
-        {
-            (data) &&
-            <>
-            <div className="w-screen h-screen flex flex-col justify-center items-center font-semibold"> 
+          <div className="w-screen h-screen flex flex-col justify-center items-center font-semibold">
             <h1>{data[0]?.displayName}</h1>
             <div>
-            <button onClick={handleUserLogout}>Logout</button>
+              <button onClick={handleUserLogout}>Logout</button>
             </div>
-            </div>
-            </>
-            
-        }
+          </div>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
