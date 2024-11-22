aaa19d8a68589bdafeb9b1f469097ec388120597
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-22 11:16:54 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ export const Home = () => {
 
     const google_data = JSON.parse(localStorage.getItem("google_login_user_data"));
     
-
     return (
         <>
             <h1>Home {google_data.displayName}</h1>
